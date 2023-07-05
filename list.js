3963267482382b49dc4288be1c68b5fa0e95af52
조세준 <sejunc0301@gmail.com>
2023-07-05 14:00:16 +0900

    페이지번호 자르고 ajax까지

---
+++ list.js
@@ -0,0 +1,30 @@
+function getPageno(){
+    // get 방식의 querystring을 읽을 수 있는 객체 생성
+    const param = new URLSearchParams(location.search);
+    const pageno = parseInt(param.get('pageno'));
+
+    // pageno가 없거나 숫자로 바꿀 수 없는 값인 경우 parseInt의 결과는 NaN
+    // Not a Number
+    // NaN을 비교할때는 isNaN() 함수를 사용해야 한다
+    
+    if(isNaN(pageno))
+        return 1;
+    else if(pageno<1)
+        return 1;
+    return pageno;
+}
+
+// 기본 매개변수(default parameter)         // 사용자는 멍멍이다..
+async function fetch(pageno=1, pagesize=10){
+    const api = 'http://sample.bmaster.kro.kr/contacts'
+    const url = `${api}?pageno=${pageno}&pagesize=${pagesize}`;
+    // $.ajax()는 병렬 처리(비동기 처리) 되는 코드 -> 언제 끝날지 모른다
+    // 비동기 코드를 리턴받는 result는 "미애레 값이 들어올 것이다"란 값을 가진다
+    // (Promise)
+    try{
+    return await $.ajax(url);
+    } catch(err){
+        console.log(err);
+        return null;
+    }
+}
